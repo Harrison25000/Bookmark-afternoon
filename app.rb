a55c61cd0054220d5e1d5ffe726fefd37f9035a6
Oliver <ofcharrison@gmail.com>
2019-02-25 15:39:21 +0000

    Got a bookmark list

--- app.rb
+++ app.rb
@@ -1,9 +1,13 @@
 require 'sinatra/base'
 
 class BookmarkWeb < Sinatra::Base
-
-  get '/' do
-    erb :index
+$bookmarks = [{name: "Bookmark 1"}, {name: "Bookmark 2"}]
+  get '/bookmarks' do
+    @bookmarks_list = []
+    $bookmarks.each do |k| 
+      @bookmarks_list << k[:name]
+    end
+    erb :bookmarks
   end
 
  run! if app_file == $0
--- spec/feature_tests/view_bookmark_spec.rb
+++ spec/feature_tests/view_bookmark_spec.rb
@@ -1,6 +1,7 @@
-feature 'As a user I would like to' do
-  scenario 'See a list of bookmarks' do
-   visit "/"
-  expect(page).to have_content 'Here are your bookmarks:'
+feature 'List of bookmarks' do
+  scenario 'We receive a list' do
+    visit '/bookmarks'
+    expect(page).to have_content 'Here are your bookmarks:'
+    expect(page).to have_content 'Bookmark 1' and 'Bookmark 2'
   end
 end
