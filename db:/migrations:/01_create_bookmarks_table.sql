dcee6aeaf420077d7ab96932206299747ac7766b
Oliver <ofcharrison@gmail.com>
2019-02-27 14:13:40 +0000

    step 8- halfway - added tester database

--- db:/migrations:/01_create_bookmarks_table.sql
+++ db:/migrations:/01_create_bookmarks_table.sql
@@ -1,1 +1,4 @@
 CREATE TABLE bookmarks(id SERIAL PRIMARY KEY, url VARCHAR(60));
+INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com')
+INSERT INTO bookmarks (URL) VALUES('http://www.destroyallsoftware.com')
+INSERT INTO bookmarks (URL) VALUES('http://www.google.com')
