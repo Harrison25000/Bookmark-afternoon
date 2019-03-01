a2d77fe49042889fd8e28be775a8fd68ce7277cf
Amina Jusic <amina_jusic@yahoo.com>
2019-03-01 15:42:19 +0000

    STep 12, added a feature test for deleting a bookmark

--- spec/feature_tests/adding_bookmarks_spec.rb
+++ spec/feature_tests/adding_bookmarks_spec.rb
@@ -2,8 +2,8 @@ feature 'Adding bookmarks' do
   scenario 'The user can add a bookmark' do
     visit '/bookmarks/new'
     fill_in 'url', with: 'http://testbookmark.com'
-    fill_in 'title' with: 'test title'
+    fill_in 'title', with: 'test title'
     click_button 'Submit'
-    expect(page).to have_content ("http://testbookmark.com", href: "http://testbookmark.com")
+    expect(page).to have_link("test title", href: "http://testbookmark.com")
   end
 end
---
+++ spec/feature_tests/deleting_a_bookmark_spec.rb
@@ -0,0 +1,7 @@
+# feature 'Deleting a bookmark' do
+#   scenario 'A user can delete a bookmark' do
+#     bookmark = Bookmark.create('http://www.facebook.com')
+#     Bookmark.delete('http://www.facebook.com')
+#     exepct(Bookmark).to eq []
+#   end
+# end
